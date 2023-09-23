4a1af4996ed182f1a53000b14badb30e540fb5f7
Joe <joe@fluidnumerics.com>
2023-09-23 06:52:07 -0400

    Add workflows

--- test/tanh_with_t.f90
+++ test/tanh_with_t.f90
@@ -1,30 +1,28 @@
 
 INTEGER FUNCTION tanh_with_t() RESULT(r)
   USE FEQParse
+  USE ISO_FORTRAN_ENV
   IMPLICIT NONE
 
   TYPE(EquationParser) :: f
   CHARACTER(LEN=1), DIMENSION(1:2) :: independentVars
-  CHARACTER(LEN=30) :: eqChar
-  REAL :: x(1:2)
-  REAL :: exact
+  CHARACTER(LEN=100) :: eqChar
+  REAL(real64) :: x(1:2)
+  REAL(real64) :: exact
 
     ! Specify the independent variables
     independentVars = (/ 'x', 't' /)
 
     ! Evaluate directly
-    exact = 0.5*(1.0 + tanh((250000.0 + 0.5*10000.0)/100000.0))
+    exact = 0.5*(1.0 - tanh((0.0 - 250000.0 - 0.5*1000000.0)/100000.0))
     ! Specify an equation string that we want to evaluate 
-    eqChar = 'f = 0.5*(1.0 - \tanh((x - 250000.0 - 0.5*t)/100000.0))'
-
-    ! Create the EquationParser object
+    eqChar = "f = 0.5*(1.0 - \tanh((x - (250000 + 0.5*t))/100000))"   ! Create the EquationParser object
     f = EquationParser(eqChar, independentVars)
 
     ! Evaluate the equation at (0,1000)
-    x = (/ 0.0, 10000.0 /) 
+    x = (/ 0.0, 1000000.0 /)
     IF( ABS(f % evaluate( x ) - exact) <= epsilon(exact) )THEN
       r = 0
-      
     ELSE
       r = 1
     ENDIF
