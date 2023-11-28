d38a070ce1a9f86fce1682be07aa3c47236b45f9
Joe <joe@fluidnumerics.com>
2023-11-28 12:53:45 -0500

    Add example that reproduces problelm

---
+++ example/scalar_function_product.f90
@@ -0,0 +1,27 @@
+program scalar_function_product
+
+  use FEQParse
+
+  implicit none
+  type(EquationParser) :: f
+  character(LEN=1),dimension(2) :: independentVars
+  character(LEN=30) :: eqChar
+  real :: x(2)
+
+  ! Specify the independent variables
+  independentVars = (/'x', 'y'/)
+
+  ! Specify an equation string that we want to evaluate
+  eqChar = 'f = \cos( 2.0*pi*x )*\cos( 2.0*pi*y )'
+
+  ! Create the EquationParser object
+  f = EquationParser(eqChar,independentVars)
+
+  ! Evaluate the equation
+  x = 0.5
+  print*, f % evaluate(x)
+
+  ! Clean up memory
+  call f % Destruct()
+
+end program scalar_function_product
