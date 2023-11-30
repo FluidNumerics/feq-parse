5a0a2da82395a21678d56d9e41413873560b45ef
Joe <joe@fluidnumerics.com>
2023-11-30 12:07:24 -0500

    increase relative error tolerance for arctan tests

--- test/atan_r3fp64.f90
+++ test/atan_r3fp64.f90
@@ -46,7 +46,7 @@ integer function atan_r3fp64() result(r)
 
   ! Evaluate the equation
   feval = f % evaluate(x)
-  if (maxval(abs(feval - fexact)) <= maxval(abs(fexact))*epsilon(1.0_real64)) then
+  if (maxval(abs(feval - fexact)) <= maxval(abs(fexact))*epsilon(1.0_real64)*10.0_real32) then
     r = 0
   else
     r = 1
